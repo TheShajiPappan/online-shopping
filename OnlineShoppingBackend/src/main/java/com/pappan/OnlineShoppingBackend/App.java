f09c89cb4e1410175a91c2222df1bf0cb799fa02
TheShajiPappan <alwinjoseth@gmail.com>
2018-07-05 10:17:28 +0530

    Initial project setup

---
+++ OnlineShoppingBackend/src/main/java/com/pappan/OnlineShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.pappan.OnlineShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
